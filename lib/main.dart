c4b846a94ba6809f6143936d9f9c3da5091dac82
Nikhil Narayanan <javakingxi@gmail.com>
2024-04-05 00:23:25 +0530

    ✨ Add Sample Table in TableScreen   - Add reset button for Turing Machine in TapeScreen   - minor improvements

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:turing_machines/models/TuringMachines.dart';
-import 'package:turing_machines/screens/TapeScreen.dart';
+import 'package:turing_machines/screens/TableScreen.dart';
 import 'package:turing_machines/testing.dart';
 
 void main() {
@@ -17,12 +17,13 @@ class MyApp extends StatelessWidget {
     TuringMachine machine = Testing.main();
     return MaterialApp(
       title: 'Turing Machine Generator',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
         useMaterial3: true,
         textTheme: GoogleFonts.sourceCodeProTextTheme(),
       ),
-      home: TapeScreen(machine: machine),
+      home: TableScreen(machine: machine),
     );
   }
 }
