5757afb9fea624982d9199739c9000ade0ca3ad6
Nikhil Narayanan <javakingxi@gmail.com>
2024-04-20 14:15:17 +0530

    Add Platform detection

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:logger/logger.dart';
 import 'package:turing_machines/models/TuringMachines.dart';
 import 'package:turing_machines/screens/TableScreen.dart';
 import 'package:turing_machines/testing.dart';
+import 'package:turing_machines/models/Targets.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
+import 'dart:io' show Platform;
 
-void main() async {
+Targets target = MyApp.detectPlatform();
+void main() {
   runApp(const MyApp());
 }
 
@@ -15,7 +20,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     TuringMachine machine = Testing.main();
-
+    //for debugginh
+    Logger().i(target.name);
     return MaterialApp(
       title: 'Turing Machine Generator',
       debugShowCheckedModeBanner: false,
@@ -26,5 +32,18 @@ class MyApp extends StatelessWidget {
       ),
       home: TableScreen(machine: machine),
     );
+  }
+
+  //detects current platform and returns an enum value representing it
+  static Targets detectPlatform() {
+    if (kIsWeb) {
+      return Targets.WEB;
+    } else if (Platform.isAndroid) {
+      return Targets.ANDROID;
+    } else if (Platform.isLinux | Platform.isMacOS | Platform.isWindows) {
+      return Targets.DESKTOP;
+    } else {
+      return Targets.UNDEFINED;
+    }
   }
 }
---
+++ lib/models/Targets.dart
@@ -0,0 +1,7 @@
+//No iOS Support
+enum Targets {
+  DESKTOP,
+  WEB,
+  ANDROID,
+  UNDEFINED,
+}
