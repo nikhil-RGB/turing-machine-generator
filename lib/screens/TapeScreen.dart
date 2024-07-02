13145d2a27892f4962bffa4db2c3e4c844d03c09
Nikhil Narayanan <javakingxi@gmail.com>
2024-07-03 01:05:12 +0530

    ✨ Implement auto-run feature

--- lib/screens/TapeScreen.dart
+++ lib/screens/TapeScreen.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: must_be_immutable
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:turing_machines/exceptions/action_exceptions.dart';
 import 'package:turing_machines/main.dart';
 import 'package:turing_machines/models/Configuration.dart';
@@ -10,18 +11,22 @@ import 'package:turing_machines/widgets/TapeWidget.dart';
 import 'package:gap/gap.dart';
 import 'package:turing_machines/models/Actions.dart' as actions1;
 
+int timeGap = 1000;
+
 class TapeScreen extends StatefulWidget {
   bool _followHead = true;
   bool automate = false;
-  int timeGap = 1000;
+
   final TuringMachine machine;
   TapeScreen({super.key, required this.machine});
   final double tape_cell_width = 50.0;
   @override
-  State<TapeScreen> createState() => specific();
+  State<TapeScreen> createState() => _TapeScreenState();
 }
 
-class specific extends State<TapeScreen> {
+class _TapeScreenState extends State<TapeScreen> {
+  final TextEditingController _timeController =
+      TextEditingController(text: "$timeGap");
   final ScrollController _sc = ScrollController();
   final ScrollController _tableScroll = ScrollController();
   @override
@@ -32,165 +37,190 @@ class specific extends State<TapeScreen> {
       platform = Targets.ANDROID;
     }
     return SafeArea(
-        child: Scaffold(
-      appBar: AppBar(title: const Text("Tape Screen"), actions: <Widget>[
-        IconButton(
+        child: PopScope(
+      canPop: !widget.automate,
+      onPopInvoked: (didPop) {
+        if (!didPop) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(
+              content: Text('Disable automatic progression to navigate back!'),
+              backgroundColor: Colors.red,
+            ),
+          );
+        }
+      },
+      child: Scaffold(
+        appBar: AppBar(title: const Text("Tape Screen"), actions: <Widget>[
+          IconButton(
             onPressed: (widget.automate)
                 ? null
                 : () {
-                    setState(() {
-                      widget.machine
-                          .softReset(); //hard-reset on tape,soft-reset on machine state.
-                    });
+                    _showTimeInputSheet(context);
                   },
             icon: Icon(
-              Icons.restart_alt_rounded,
+              Icons.schedule_outlined,
               color: (widget.automate) ? Colors.grey : Colors.blue,
-            )),
-      ]),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        //MainAxisAlignment should be start for smaller screens,
-        //smaller screens should also increase gap between table and tape.
-        children: [
-          buildTable(platform: platform),
-          Gap((platform == Targets.ANDROID) ? 45 : 22),
-          //Padding on the left and right of the tape widget prevents the tape from touching the ends of the window
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8.0),
-            child: TapeWidget(
-              cell_width: widget.tape_cell_width,
-              tape: widget.machine.tape,
-              s_controller: _sc,
             ),
           ),
-          const Gap(20),
-          Text(
-            "Current pointer: ${widget.machine.tape.pointer}",
-            style: const TextStyle(
-                color: Colors.blue, fontWeight: FontWeight.bold),
-          ),
-          const Gap(2),
-          Text(
-            "Current m-config: ${widget.machine.current_config}",
-            style: const TextStyle(
-                color: Colors.blue, fontWeight: FontWeight.bold),
-          ),
-          const Gap(2),
-          Text(
-            "Scanned Symbol: ${scannedSymbol(widget.machine.tape.tape[widget.machine.tape.pointer])}",
-            style: const TextStyle(
-                color: Colors.blue, fontWeight: FontWeight.bold),
-          ),
-          const Gap(2),
-          Text(
-            "Iteration number: ${widget.machine.iterations}",
-            style: const TextStyle(
-                color: Colors.blue, fontWeight: FontWeight.bold),
-          ),
           const Gap(5),
-          SizedBox(
-            width: 220,
-            child: CheckboxListTile(
-              title: Text(
-                "Follow Head Pointer",
-                style: TextStyle(
-                  fontSize: 14,
-                  fontWeight: FontWeight.bold,
+          IconButton(
+              onPressed: (widget.automate)
+                  ? null
+                  : () {
+                      setState(() {
+                        widget.machine
+                            .softReset(); //hard-reset on tape,soft-reset on machine state.
+                      });
+                    },
+              icon: Icon(
+                Icons.restart_alt_rounded,
+                color: (widget.automate) ? Colors.grey : Colors.blue,
+              )),
+        ]),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          //MainAxisAlignment should be start for smaller screens,
+          //smaller screens should also increase gap between table and tape.
+          children: [
+            buildTable(platform: platform),
+            Gap((platform == Targets.ANDROID) ? 45 : 22),
+            //Padding on the left and right of the tape widget prevents the tape from touching the ends of the window
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: TapeWidget(
+                cell_width: widget.tape_cell_width,
+                tape: widget.machine.tape,
+                s_controller: _sc,
+              ),
+            ),
+            const Gap(20),
+            Text(
+              "Current pointer: ${widget.machine.tape.pointer}",
+              style: const TextStyle(
+                  color: Colors.blue, fontWeight: FontWeight.bold),
+            ),
+            const Gap(2),
+            Text(
+              "Current m-config: ${widget.machine.current_config}",
+              style: const TextStyle(
+                  color: Colors.blue, fontWeight: FontWeight.bold),
+            ),
+            const Gap(2),
+            Text(
+              "Scanned Symbol: ${scannedSymbol(widget.machine.tape.tape[widget.machine.tape.pointer])}",
+              style: const TextStyle(
+                  color: Colors.blue, fontWeight: FontWeight.bold),
+            ),
+            const Gap(2),
+            Text(
+              "Iteration number: ${widget.machine.iterations}",
+              style: const TextStyle(
+                  color: Colors.blue, fontWeight: FontWeight.bold),
+            ),
+            const Gap(5),
+            SizedBox(
+              width: 220,
+              child: CheckboxListTile(
+                title: Text(
+                  "Follow Head Pointer",
+                  style: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold,
+                    color: (widget._followHead) ? Colors.blue : Colors.black,
+                  ),
+                ),
+                value: widget._followHead,
+                controlAffinity: ListTileControlAffinity.leading,
+                secondary: Icon(
+                  Icons.follow_the_signs,
                   color: (widget._followHead) ? Colors.blue : Colors.black,
                 ),
+                activeColor: Colors.blue,
+                onChanged: (bool? val) {
+                  if (val == null) {
+                    return;
+                  }
+                  setState(() {
+                    widget._followHead = val;
+                  });
+                },
               ),
-              value: widget._followHead,
-              controlAffinity: ListTileControlAffinity.leading,
-              secondary: Icon(
-                Icons.follow_the_signs,
-                color: (widget._followHead) ? Colors.blue : Colors.black,
-              ),
-              activeColor: Colors.blue,
-              onChanged: (bool? val) {
-                if (val == null) {
-                  return;
-                }
-                setState(() {
-                  widget._followHead = val;
-                });
-              },
             ),
-          ),
-          const Gap(5),
+            const Gap(5),
 
-          //Put the automate checkbox here, maybe.
-          SizedBox(
-            width: 220,
-            child: CheckboxListTile(
-              title: Text(
-                "Automate Progression",
-                style: TextStyle(
-                  fontSize: 14,
-                  fontWeight: FontWeight.bold,
+            //Put the automate checkbox here, maybe.
+            SizedBox(
+              width: 220,
+              child: CheckboxListTile(
+                title: Text(
+                  "Automate Progression",
+                  style: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold,
+                    color: (widget.automate) ? Colors.blue : Colors.black,
+                  ),
+                ),
+                value: widget.automate,
+                controlAffinity: ListTileControlAffinity.leading,
+                secondary: Icon(
+                  Icons.auto_mode_outlined,
                   color: (widget.automate) ? Colors.blue : Colors.black,
                 ),
+                activeColor: Colors.blue,
+                onChanged: (bool? value) {
+                  if (value == null) {
+                    return;
+                  }
+                  setState(() {
+                    widget.automate = value;
+                  });
+                  if (widget.automate) {
+                    automateProgression();
+                  }
+                },
               ),
-              value: widget.automate,
-              controlAffinity: ListTileControlAffinity.leading,
-              secondary: Icon(
-                Icons.auto_mode_outlined,
-                color: (widget.automate) ? Colors.blue : Colors.black,
-              ),
-              activeColor: Colors.blue,
-              onChanged: (bool? value) {
-                if (value == null) {
-                  return;
-                }
-                setState(() {
-                  widget.automate = value;
-                });
-                if (widget.automate) {
-                  automateProgression();
-                }
-              },
             ),
-          ),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: (widget.automate)
-            ? null
-            : () {
-                bool result = true;
-                try {
-                  widget.machine.stepIntoConfig();
-                } on InvalidLookupException {
-                  result = false;
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(
-                      content: Text('Turing Machine has Halted!'),
-                      backgroundColor: Colors.red,
-                    ),
-                  );
-                } on TapeOperationException catch (e) {
-                  result = false;
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(
-                      content: Text(e.message),
-                      backgroundColor: Colors.red,
-                    ),
-                  );
-                }
-                if (result) {
-                  setState(() {});
-                  if (widget._followHead) {
-                    _sc.animateTo(
-                      (widget.tape_cell_width * widget.machine.tape.pointer),
-                      duration: const Duration(milliseconds: 200),
-                      curve: Curves.ease,
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: (widget.automate)
+              ? null
+              : () {
+                  bool result = true;
+                  try {
+                    widget.machine.stepIntoConfig();
+                  } on InvalidLookupException {
+                    result = false;
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(
+                        content: Text('Turing Machine has Halted!'),
+                        backgroundColor: Colors.red,
+                      ),
+                    );
+                  } on TapeOperationException catch (e) {
+                    result = false;
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text(e.message),
+                        backgroundColor: Colors.red,
+                      ),
                     );
                   }
-                }
-              },
-        backgroundColor: (widget.automate) ? Colors.grey : Colors.blue,
-        child: const Icon(Icons.play_arrow_outlined),
+                  if (result) {
+                    setState(() {});
+                    if (widget._followHead) {
+                      _sc.animateTo(
+                        (widget.tape_cell_width * widget.machine.tape.pointer),
+                        duration: const Duration(milliseconds: 200),
+                        curve: Curves.ease,
+                      );
+                    }
+                  }
+                },
+          backgroundColor: (widget.automate) ? Colors.grey : Colors.blue,
+          child: const Icon(Icons.play_arrow_outlined),
+        ),
       ),
     ));
   }
@@ -320,7 +350,7 @@ class specific extends State<TapeScreen> {
   Future automateProgression() async {
     while (widget.automate) {
       await Future.delayed(
-        Duration(milliseconds: widget.timeGap),
+        Duration(milliseconds: timeGap),
         () async {
           if (widget.automate) {
             bool result = true;
@@ -363,5 +393,72 @@ class specific extends State<TapeScreen> {
         },
       );
     }
+  }
+
+  void _showTimeInputSheet(BuildContext context) {
+    showModalBottomSheet(
+        context: context,
+        useSafeArea: true,
+        isScrollControlled: true,
+        enableDrag: false,
+        isDismissible: true,
+        builder: (context) {
+          return Padding(
+            padding: EdgeInsets.only(
+              bottom: MediaQuery.of(context).viewInsets.bottom,
+            ),
+            child: Container(
+              height: 230,
+              padding: const EdgeInsets.only(
+                bottom: 8.0,
+                top: 15,
+                left: 15,
+                right: 15,
+              ),
+              child: Center(
+                  child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const Text("Time gap between each automatic iteration: (ms)"),
+                  const Gap(15),
+                  TextField(
+                    controller: _timeController,
+                    maxLength: 6,
+                    keyboardType: TextInputType.number,
+                    inputFormatters: <TextInputFormatter>[
+                      FilteringTextInputFormatter.allow(RegExp(r'[0-9]')),
+                    ],
+                  ),
+                  const Gap(15.0),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 5.0),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          child: const Text("Cancel"),
+                        ),
+                        const Gap(7.0),
+                        ElevatedButton(
+                          onPressed: () {
+                            timeGap = int.parse(_timeController.text);
+
+                            Navigator.pop(context);
+                          },
+                          child: const Text("Ok"),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              )),
+            ),
+          );
+        });
   }
 }
